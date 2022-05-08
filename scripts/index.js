7c7ef1f15da227855a5a1f5ba5b55ce4b4438951
Maxtish <rd-max@ya.ru>
2022-05-08 20:40:14 +0300

    fix all

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,23 @@
 const doc = document;
 const log = console.log;
 
-const boxInp1 = doc.querySelector('.samples__item');
+const nameInput = doc.querySelector('#name-input');
+const namelabel = doc.querySelector('#name-label');
+const surnameInput = doc.querySelector('#surname-input');
+const surnamelabel = doc.querySelector('#surname-label');
+const emailInput = doc.querySelector('#email-input');
+const emaillabel = doc.querySelector('#emai-label');
 
-log(boxInp1);
+function valid(inpute, label) {
+  inpute.addEventListener('input', function (e) {
+    if (inpute.value.length > 0) {
+      label.classList.add('samples__label_valid');
+    } else {
+      label.classList.remove('samples__label_valid');
+    }
+  });
+}
+
+valid(nameInput, namelabel);
+valid(surnameInput, surnamelabel);
+valid(emailInput, emaillabel);
